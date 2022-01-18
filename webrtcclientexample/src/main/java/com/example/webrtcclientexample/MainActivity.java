96e5090be21fb5f66f5391c8e9311fb0f6058a7e
Aron List <aronotmar@live.nl>
2022-01-18 22:24:42 +0100

    small improvements and lots of comments

--- webrtcclientexample/src/main/java/com/example/webrtcclientexample/MainActivity.java
+++ webrtcclientexample/src/main/java/com/example/webrtcclientexample/MainActivity.java
@@ -51,15 +51,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //I'm using layout binding for easy access to the surfaceView
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //Initialization of the connectionFactory using basic settings, nothing special
         PeerConnectionFactory.InitializationOptions initOptions = PeerConnectionFactory.InitializationOptions.builder(this.getApplicationContext())
                 .createInitializationOptions();
         PeerConnectionFactory.initialize(initOptions);
 
+        //This eglBase needs to be used everywhere you need it. Using a different one for the surfaceview and peerConnection(factory)
+        //will result in nothing showing on the screen for some reason. They should really have turned this into a singleton
         eglBase = EglBase.create();
 
+        //Basic video encoder and decoder. You can mess around with these to do things with codecs. But it's rather complicated.
         VideoEncoderFactory encoderFactory = new DefaultVideoEncoderFactory(eglBase.getEglBaseContext(), true, false);
         VideoDecoderFactory decoderFactory = new DefaultVideoDecoderFactory(eglBase.getEglBaseContext());
 
@@ -73,9 +78,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        //This surfaceview is not the regular one, it's special and comes from the library
+        //It needs to be initialised once, and only once. Otherwise it'll throw an exception
         binding.surfaceView.release();
         binding.surfaceView.init(eglBase.getEglBaseContext(), null);
         binding.surfaceView.setEnableHardwareScaler(true);
+
+        //dispose of any previously made connections and create a new one
         if (connection != null) {
             connection.dispose();
         }
@@ -101,8 +110,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doOffer(PeerConnection connection) {
+        //You could use these to limit the streams resolution and or framerate I think.
+        //The RTC lib should automatically handle staying "within the constraints" if you set any
         MediaConstraints sdpMediaConstraints = new MediaConstraints();
 
+        //Create an offer. Several things to note here. I'm using synchronised blocks and waiting to
+        //make sure the ICE candidate gathering is completed before sending the offer. Also note I'm not
+        //using the sessionDescription that is passed to the callback, but the connection.getLocalDescription()
+        //as the payload of the actual request. That one has the gathered ICE candidates in it which is required
         connection.createOffer(new SimpleSdpObserver() {
             @Override
             public void onCreateSuccess(SessionDescription sessionDescription) {
@@ -128,6 +143,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private PeerConnection createPeerConnection(PeerConnectionFactory factory) {
+        //Create the peerconnection. Lots of callbacks. Only the OnIceGatheringChange and onTrack
+        //are actually interesting. I have not tried without a stun server, but technically it's
+        //only required if you are not on the same local network as the camera source
         ArrayList<PeerConnection.IceServer> iceServers = new ArrayList<>();
         String URL = "stun:stun.l.google.com:19302";
         iceServers.add(PeerConnection.IceServer.builder(URL).createIceServer());
@@ -155,6 +173,8 @@ public class MainActivity extends AppCompatActivity {
                 if (iceGatheringState == PeerConnection.IceGatheringState.COMPLETE) {
                     synchronized (lock) {
                         Log.d(TAG, "gathering complete, notifying...");
+                        //this wakes up the thread that created the offer and will continue sending
+                        //the request after on line 131
                         lock.notify();
                     }
                 }
@@ -196,32 +216,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAddTrack(RtpReceiver rtpReceiver, MediaStream[] mediaStreams) {
                 Log.d(TAG, "onAddStream: " + mediaStreams.length);
-                for (MediaStream stream : mediaStreams) {
-                    if (stream.videoTracks.size() > 0) {
-                        currentTrack = stream.videoTracks.get(0);
-                        currentTrack.addSink(binding.surfaceView);
-                        return;
-                    }
-                }
+
+                //once the connection has been set-up a track will be added on the other side
+                //which will show up here. Adding the surfaceView as a sink will cause the track
+                //to be rendered on screen!
+                if (mediaStreams.length < 1)
+                    return;
+
+                if (mediaStreams[0].videoTracks.size() < 1)
+                    return;
+
+                currentTrack = mediaStreams[0].videoTracks.get(0);
+                currentTrack.addSink(binding.surfaceView);
             }
         };
 
+        //Create the peerconnection and add a transceiver of ReceiveOnly
+        //This will tell the other side what we want to be receiving
+        //You can also add a transceiver for audio if you would like to receive an audio stream as well
         PeerConnection peerConnection = factory.createPeerConnection(iceServers, pcObserver);
         assert peerConnection != null;
         peerConnection.addTransceiver(MediaStreamTrack.MediaType.MEDIA_TYPE_VIDEO,
-                new RtpTransceiver.RtpTransceiverInit(RtpTransceiver.RtpTransceiverDirection.RECV_ONLY));
-        peerConnection.addTransceiver(MediaStreamTrack.MediaType.MEDIA_TYPE_AUDIO,
                 new RtpTransceiver.RtpTransceiverInit(RtpTransceiver.RtpTransceiverDirection.RECV_ONLY));
         return peerConnection;
     }
 
     private void handleRequest(JSONObject message, PeerConnection connection) {
+        // Send the offer and handle the incoming answer
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.POST,
                 CAM_URL,
                 message,
                 response -> {
                     try {
+                        //Here we check if we actually got an answer. Create a sessionDescription if
+                        //we did and add this as the remoteDescription to he peerconnection.
+                        //If that is successful any tracks added to the connection from the other side
+                        //will start generating onAddTrack events here.
                         String type = response.getString("type");
                         if (!type.equals(SessionDescription.Type.ANSWER.canonicalForm()))
                             return;
@@ -236,8 +267,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, error.toString());
                 }
         );
-
-        request.setRetryPolicy(new DefaultRetryPolicy(20000, 1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
         queue.add(request);
     }
